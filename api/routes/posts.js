0071c5df6d6b9ce33dc10d7be3d99e0e38b1f2e0
Frank Martinez <frankie.martinez@gmail.com>
2020-01-19 00:31:20 -0800

    ✨ post
    route

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,6 +1,6 @@
 import express from "express";
 import db from "../../connection.js";
-// import { errorObject,errorBackup } from "../../middleware/errorHandling";
+import { errorObject,errorBackup } from "../../middleware/errorHandling";
 
 const posts = express.Router();
 
@@ -18,6 +18,19 @@ posts.get('/', async (req, res) => {
         res.status(500).json({ message: "posts could not be retrieved.", error:error });
     }
 });
+
+posts.post("/", async (req, res) => {
+    if (!req.body.title || !req.body.body) return res.status(400).json({ message: "Please include an id" })
+    try {
+        const post = await db("posts")
+        .returning([ "id" ])
+        .insert(req.body);
+        return res.status(200).json({ message:"Post created", post }); 
+    } catch (error) {
+        const message = errorObject[error.errno] || errorBackup;
+        res.status(500).json({ message, error });
+    }
+})
 
 
 export default posts;
