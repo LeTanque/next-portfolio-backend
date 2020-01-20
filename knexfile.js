5e465dadfca7ef1429d4596d848cc67a6cb8ac9b
Frank Martinez <frankie.martinez@gmail.com>
2020-01-19 21:01:08 -0800

    🗃 Movin over to pg

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,32 @@
+const dbConnection = process.env.DATABASE_URL || "localhost";
 
 
 module.exports = {
 
     development: {
-        client: 'sqlite3',
-        connection: {
-            filename: './data/portfolio.db3',
-        },
-        useNullAsDefault: true,
+        client: 'pg',
+        connection: dbConnection,
         migrations: {
             directory: './data/migrations',
+            tableName: 'knex_migrations',
         },
         seeds: {
             directory: './data/seeds',
-        }
+        },
+        useNullAsDefault: true,
     },
 
     production: {
-        client: 'sqlite3',
-        connection: {
-            filename: './data/portfolio.db3',
-        },
-        useNullAsDefault: true,
+        client: 'pg',
+        connection: dbConnection,
         migrations: {
             directory: './data/migrations',
+            tableName: 'knex_migrations',
         },
         seeds: {
             directory: './data/seeds',
-        }
+        },
+        useNullAsDefault: true,
     }
 
 };
